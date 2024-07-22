f0f00298aedc390aa24714650533c58f1eabd98f
niko <pmpiga1@gmail.com>
2024-07-22 02:12:41 -0400

    update variable names

--- tests/test1.nim
+++ tests/test1.nim
@@ -4,19 +4,19 @@ import unittest
 import brotli
 
 const
-  data = "lazy test daaaaaataaaaaaa ssddddvfggggfdks meow :3c"
-  compressed =
+  simpl = "lazy test daaaaaataaaaaaa ssddddvfggggfdks meow :3c"
+  simplCompressed =
     "\x1b2\x00\xf8\xc5\xf2\x96\xea\x85\xaei\xed\xe6\xbc\x80\x09g\x0c\xd8\x80\x13I\xc2\x01y\xb0\x81\xf0B \x9d\x0d\xfe\x17S\x11\x9b\x81-O\xb4\x1f\xea\xc6\xac\xeb\"\xa7\x02"
 
 suite "encoding":
   test "simple test":
-    let compressed = compressBrotli(data)
-    check compressed == compressed
+    let compressed = compressBrotli(simpl)
+    check compressed == simplCompressed
 
 suite "decoding":
   test "simple test":
-    let decompressed = decompressBrotli(compressed)
-    check decompressed == data
+    let decompressed = decompressBrotli(simplCompressed)
+    check decompressed == simpl
 
   test "reallocation":
     let
