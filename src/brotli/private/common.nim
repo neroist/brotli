52f60e1cd6038501f4fe88a3e69277ba79608380
niko <pmpiga1@gmail.com>
2024-07-22 01:44:44 -0400

    make private/common only compile when using static compilation

--- src/brotli/private/common.nim
+++ src/brotli/private/common.nim
@@ -1,9 +1,13 @@
 ## Helper module to make sure `csrc/common/*.c` is only compiled once
 ## as compiling it twice causes issues
 
-when not declared(brotliPrivCommonCompiled):
-  {.compile: ("./../csrc/common/*.c", "$#.o").}
+const useStaticCompilation =
+  not (defined(useBrotliEncDll) or defined(useBrotliDecDll) or defined(useBrotliAllDll) or defined(useBrotliDll))
 
-const brotliPrivCommonCompiled* = true
-  ## Marker indicating whether or not `common/*.c` has been compiled.
-  ## The value doesn't matter, only the constant's declaration does.
+when useStaticCompilation or defined(nimdoc):
+  when not declared(brotliPrivCommonCompiled):
+    {.compile: ("./../csrc/common/*.c", "$#.o").}
+
+  const brotliPrivCommonCompiled* = true
+    ## Marker indicating whether or not `common/*.c` has been compiled.
+    ## The value doesn't matter, only the constant's declaration does.
